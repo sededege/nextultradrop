5dec26e391e8d1c533c4171090461e6e48e30509
Sebastian Gonzalez <rankzjehz@MacBook-Pro-de-Sebastian.local>
2023-02-25 21:28:12 -0300

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,14 +20,14 @@ export default function Home({ resultado }) {
   return (
     <Layout pagina="Inicio">
       <div className="w-full h-full grid sm:grid-cols-1 md:grid-cols-3 lg:grid-colds-4 gap-10">
-        {data.map((e) => (
+       {/*  {data.map((e) => (
          <Card 
          img={e.attributes.Image.data.attributes.url}
          title={e.attributes.Title}
          description={e.attributes.Description}
          me={e.attributes.ME}
          />
-        ))}
+        ))} */}
       </div>
     </Layout>
   );
