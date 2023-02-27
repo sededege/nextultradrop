fb9f2a7fed34927b55dde502bb2df4b9db90be8b
Sebastian Gonzalez <rankzjehz@MacBook-Pro-de-Sebastian.local>
2023-02-27 04:23:57 -0300

    first

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['maker.macks.gg','ultradroptest.s3.amazonaws.com'],
+    domains: ['maker.macks.gg','ultradroptest.s3.amazonaws.com','54.147.7.66'],
   }
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 import Card from "@/components/Card";
 
 export async function getServerSideProps() {
-  const url = "http://localhost:1337/api/ultradrops?populate=*";
+  const url = "http://54.147.7.66:1337/api/ultradrops?populate=*";
   const respuesta = await fetch(url);
   const resultado = await respuesta.json();
   return {
@@ -17,17 +17,18 @@ export async function getServerSideProps() {
 
 export default function Home({ resultado }) {
   const [data] = useState(resultado.data);
+  console.log(data)
   return (
     <Layout pagina="Inicio">
       <div className="w-full h-full grid sm:grid-cols-1 md:grid-cols-3 lg:grid-colds-4 gap-10">
-       {/*  {data.map((e) => (
+        {data.map((e) => (
          <Card 
-         img={e.attributes.Image.data.attributes.url}
-         title={e.attributes.Title}
+         img={`http://54.147.7.66:1337${e.attributes.Image.data[0].attributes.url}`}
+          title={e.attributes.Title}
          description={e.attributes.Description}
          me={e.attributes.ME}
          />
-        ))} */}
+        ))}
       </div>
     </Layout>
   );
